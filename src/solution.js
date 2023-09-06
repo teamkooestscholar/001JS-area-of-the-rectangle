b1f0c984f8e03dbb3cd65357308e83f3b19b2a47
cortezrc6 <82867552+cortezrc6@users.noreply.github.com>
2023-09-06 19:35:13 +0800

    Calculate the area with perimeter, diagonal and loop

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,28 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+function calculateDiagonal(length, width) {
+  return Math.sqrt(length ** 2 + width ** 2);
 }
 
+function calculateAndDisplayRectangleProperties() {
+  const length = getValidInput("Enter the length of the rectangle:");
+  const width = getValidInput("Enter the width of the rectangle:");
+
+  const area = calculateRectangleArea(length, width);
+  const perimeter = calculateRectanglePerimeter(length, width);
+  const diagonal = calculateDiagonal(length, width);
+
+  alert(`The area of the rectangle is: ${area}`);
+  alert(`The perimeter of the rectangle is: ${perimeter}`);
+  alert(`The length of the diagonal is: ${diagonal.toFixed(2)}`);
+}
+
+
+while (true) {
+  calculateAndDisplayRectangleProperties();
+
+  const continueInput = prompt("Do you want to calculate properties for another rectangle? (yes/no)").toLowerCase();
+  if (continueInput !== "yes") {
+    break;
+  }
+}
+
+alert("Goodbye!");
