3a4675be9202710c22bcaee24c6492a39d303482
Jonash Gania (Nash) <123818663+JonashGania@users.noreply.github.com>
2023-09-06 13:51:03 +0000

    Added functions

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,46 @@
 // Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+const calculateRectangleArea = (w, l) => {
+  let area;
+
+  if(isNaN(w) || isNaN(l)){
+    return 'Error: Invalid input'
+  } else {
+    area = w * l;
+  }
+  return area;
 }
 
+// Function to calculate the perimeter of a rectangle
+
+const calculateRectanglePerimeter = (w, l) => {
+  let perimeter;
+
+  if(isNaN(w) || isNaN(l)){
+    return 'Error: Invalid Input'
+  } else {
+    perimeter = 2 * (w + l);
+  }
+  return perimeter;
+}
+
+// Function to calculate the length of the diagonal
+
+const calculateDiagonalLength = (a, b) => {
+  let c;
+
+  if(isNaN(a) || isNaN(b)){
+    return 'Error: Invalid Input'
+  } else {
+    a *= a;
+    b *= b;
+
+    let sum = a + b;
+    c = Math.sqrt(sum);
+  }
+
+  return c
+}
+
+console.log(calculateRectangleArea(10,10));
+console.log(calculateRectanglePerimeter(10,10));
+console.log(calculateDiagonalLength(9,7));
