798205350c9925e5c298ae09ee486d5419969fd1
jlfx09 <142736901+jlfx09@users.noreply.github.com>
2023-09-07 00:17:10 +0800

    git add .

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,36 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
-}
 
+function calculateRectangleArea(width,height) {
+  // formula of the area
+  let area = width * height;
+
+  // Return the Area
+  return area;
+}
+  function calculateRectanglePerimeter(width,height) {
+  let perimeter = (length+width) * 2;
+
+  // Return the Perimeter
+  return perimeter;
+}
+  function calculateRectangleDiagonal(width,height){
+  let diagonal =(length^2 + width^2)**2;
+
+  // Return the Diagonal
+  return diagonal;
+}
+  //Prompt the user for input
+  const width = parseFloat(prompt("Please input the Width of the Rectangle: "));
+  const height = parseFloat(prompt("Please input the height of the Rectangle: "));
+  //Check if the Width and Height are Numerical Values
+
+  if (!isNaN(width) && !isNaN(height)) {
+    const area = calculateRectangleArea(width,height).toFixed(2);
+    console.log (`The area of the rectangle is: ${area}`)
+    const perimeter = calculateRectanglePerimeter(width,height).toFixed(2);
+    console.log (`The Perimeter of the rectangle is: ${perimeter}`)
+    const diagonal = calculateRectangleDiagonal(width,height).toFixed(2);
+    console.log (`The Diagonal of the rectangle is: ${diagonal}`)
+    
+  } else {
+    console.log("Invalid input for Width and Height. Please enter valid numerical values.")
+  }
