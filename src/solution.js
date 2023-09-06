837d4f6b4370e356feb9f135af461b35e4f416f0
cortezrc6 <82867552+cortezrc6@users.noreply.github.com>
2023-09-06 19:36:11 +0800

    update

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,24 @@
+function getValidInput(promptText) {
+  let value;
+  while (true) {
+    value = parseFloat(prompt(promptText));
+    if (!isNaN(value) && value > 0) {
+      break;
+    } else {
+      alert("Please enter a valid positive number.");
+    }
+  }
+  return value;
+}
+
+function calculateRectangleArea(length, width) {
+  return length * width;
+}
+
+function calculateRectanglePerimeter(length, width) {
+  return 2 * (length + width);
+}
+
 function calculateDiagonal(length, width) {
   return Math.sqrt(length ** 2 + width ** 2);
 }
