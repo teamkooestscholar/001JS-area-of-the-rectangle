800a1edcb1072af9c934cf551386204e8a8cc895
Noey Ignacio <hello@noeyislearning.dev>
2023-09-06 16:46:23 +0800

    built(*): Revert to original tasks

--- src/solution.js
+++ src/solution.js
@@ -1,25 +1,5 @@
-function calculateRectangleArea(w, l) {
-  var a = w + l;
-  var a2 = a * 2;
-  if (isNaN(w, l)) {
-    return 'ERROR: not a Number!';
-  }
-  return a2;
+// Function to calculate the area of a rectangle
+function calculateRectangleArea(_, _) {
+  // Put your code here
 }
-
-function calculateDiagonal(w , l){
-  var a1 = w**2;
-  var a2 = l**2;
-  var a3 = a1 + a2;
-  var total = Math.sqrt(a3);
-
-  if (isNaN(w, l)) {
-    return 'ERROR: not a Number!';
-  }
-  return total;
-}
-
-console.log(calculateRectangleArea('5', '5'));
-
-console.log(calculateDiagonal(5, 5))
 
