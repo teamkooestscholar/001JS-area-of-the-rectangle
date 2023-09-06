0c1ee3877bb0c480bedac1db88516204b42bb5a3
lindraaa <chancruz739@gmail.com>
2023-09-06 22:47:48 +0800

    adada

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,52 @@
 // Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+function getting_area(l,w){
+  let a;
+  if (isNaN(l) || isNaN(w)){
+    return "error";
+    
+  }
+  else{
+     a = l*w;
+    
+  }
+  return a;
 }
+
+function getting_perimeter(l, w){
+  let b;
+  if (isNaN(l) || isNaN(w)){
+    return "error";
+    
+  }
+  else{
+    b = 2 * (l + w);
+    
+    
+  }
+  return b;
+}
+ 
+
+
+function getting_lengdig(l,w){
+  let c,d,e;
+  if (isNaN(l) || isNaN(w)){
+    return "error";
+    
+  }
+  else{
+  c = Math.pow(l,2);
+  d = Math.pow(w,2);
+  e = Math.sqrt(c + d);
+ 
+    
+  }
+  return e;
+}
+ 
+ 
+let l =5;
+let w = 5;
+
+console.log(getting_area(l,w),(" "),getting_perimeter(l,w),(" "),getting_lengdig(l,w));
 
