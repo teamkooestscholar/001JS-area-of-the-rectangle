b4e909ac7db335bcd7b558925ea79a64ff42c11f
gemoftruesight <kcdsanchezz@gmail.com>
2023-09-07 21:29:32 +0800

    update

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,56 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+// Calculate the area of a rectangle
+function areaofrect(lng,wdt){
+  let a;
+  if (isNaN(lng) || isNaN(wdt)){
+    return "error";
+    
+  }
+  else{
+     a = lng*wdt;
+    
+  }
+  return a;
 }
 
+// Calculate and display the perimiter of the rectangle.
+
+function rectpermtr(lng, wdt){
+  let b;
+  if (isNaN(lng) || isNaN(wdt)){
+    return "error";
+    
+  }
+  else{
+    b = 2 * (lng + wdt);
+    
+    
+  }
+  return b;
+}
+ 
+// Calculate and display the length of the diagonal
+
+
+function diagnlngth(lng,wdt){
+  let x,y,z;
+  if (isNaN(lng) || isNaN(wdt)){
+    return "error";
+    
+  }
+  else{
+  x = Math.pow(lng,2);
+  y = Math.pow(wdt,2);
+  z = Math.sqrt(x + y);
+  }
+  return z;
+}
+ 
+ 
+let lng =10;
+let wdt = 15;
+
+console.log(
+  "Area:", areaofrect(lng, wdt),
+  "Perimeter:", rectpermtr(lng, wdt),
+  "Diagonal Length:", diagnlngth(lng, wdt)
+  );
