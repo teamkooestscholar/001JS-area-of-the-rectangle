4ca2bae653930704ddb1549ee3f098ca521ea5c4
Cy12swe <104562626+Cy12swe@users.noreply.github.com>
2023-09-07 20:25:09 +0800

    Created a function to calculate the area of a rectangle as well as its diagonal lenght

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,29 @@
 // Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+function calculateRectangleArea(w, h) {
+  let w, h;
+  
+  while (isNaN(w)) {
+    let winput = prompt("Enter the width of the rectangle");
+    w = parseFloat(input);
+    if(isNaN(w)){
+      console.error("Invalid input. Please enter a valid number for width.");
+    }
+  }
+
+while (isNaN(h)) {
+  let hinput = prompt("Enter the height of the rectangle");
+  h = parseFloat(input);
+  if(isNaN(h)){
+    console.error("Invalid input. Please enter a valid number for height.");
+  }
+}
+ let a = w * h;
+
+ let diagonal = Math.sqrt(Math.pow(h, 2) +
+ Math.pow(w, 2));
+
+ console.log("The area of rectangle is: " + a);
+ console.log("The diagonal of the rectangle is: " + diagonal)
 }
 
+function calculateRectangleArea();
