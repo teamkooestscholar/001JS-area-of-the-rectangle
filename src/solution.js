ebde93c57475e76466145596f0a08c51326e69bf
kellydeguzman <kellydeguzman15@gmail.com>
2023-09-08 21:52:20 +0800

    act

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,50 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+// 001 Function to calculate the area of a rectangle
+function calcRectArea(l,w) 
+{
+  let a;
+  if (isNaN(l) || isNaN(w)){
+    return "Error: An unexpected error has occurred." ;
+    
+  }
+  else{
+     a = l*w;
+  }
+  return a;
 }
+
+function calcPerimeter(l, w)
+{
+  let p;
+  if (isNaN(l) || isNaN(w))
+  {
+    return "Error: An unexpected error has occurred." ;
+    
+  }
+  else{
+   p = 2 * (l + w);
+
+      }
+  return p;
+}
+ 
+function calcLengthDiagonal(l,w){
+  let c,d,e;
+  if (isNaN(l) || isNaN(w)){
+    return "Error: An unexpected error has occurred." ;
+    
+  }
+  else{
+  c = Math.pow(l,2);
+  d = Math.pow(w,2);
+  e = Math.sqrt(c + d);
+      }
+  return e;
+}
+
+let l =5;
+let w = 5;
+
+console.log(calcRectArea(l,w),(" "),calcPerimeter(l,w),(" "),calcLengthDiagonal(l,w)); 
+
+
 
