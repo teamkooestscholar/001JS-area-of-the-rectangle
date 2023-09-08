5a0c58d1c34ed32ded08c28a989d947657bfa9a6
ChristianPaulo24 <123344269+ChristianPaulo24@users.noreply.github.com>
2023-09-08 17:03:45 +0800

    solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,16 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
-}
+function calculateRectangleArea(w, h) 
+{
+    if (isNaN(w) || isNaN(h))
+    {
+        console.log("Error: Please enter valid numbers for width and height.");
+        return;
+    }
 
+    const a = w * h;
+    const p = 2 * (w + h);
+    const d = Math.sqrt(w**2 + h**2);
+
+    console.log("Area of the rectangle: " + a);
+    console.log("Perimeter of the rectangle: " + p);
+    console.log("Length of the diagonal: " + d);
+}
