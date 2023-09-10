7f726e654712b6b55b8946c5678a6b28de6edac8
DarrellBagsic <123875127+DarrellBagsic@users.noreply.github.com>
2023-09-10 20:34:35 +0800

    areapush

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,30 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+console.log("Area, perimeter, and diagonal of a rectangle calculator");
+
+function calculateRectangleProperties() {
+  let wInput = prompt("Enter the width:");
+  let hInput = prompt("Enter the height:");
+
+  if (!wInput || !hInput) {
+    console.log("Error: Please enter valid values.");
+    return;
+  }
+
+  let w = parseFloat(wInput);
+  let h = parseFloat(hInput);
+
+  if (isNaN(w) || isNaN(h)) {
+    console.log("Error: Please enter valid numbers.");
+    return;
+  }
+
+  let a = w * h;
+  let perimeter = 2 * (w + h);
+  let diagonal = Math.sqrt(w ** 2 + h ** 2);
+
+  console.log(`Rectangle Properties:`);
+  console.log(`- Area: ${a}`);
+  console.log(`- Perimeter: ${perimeter}`);
+  console.log(`- Length of Diagonal: ${diagonal}`);
 }
 
+calculateRectangleProperties();
