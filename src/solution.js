ec273ee6fe3d6b429034f4a201af3238efd46588
tobywan1 <shantoby00@yahoo.com>
2023-09-12 23:29:53 +0800

    Update solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,20 @@
 // Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
-}
+function calculateRectangleArea(width, height) {
+  if (!isNaN(width) && !isNaN(height)) {
+    const area = width * height;
 
+    return area;
+  } else {
+  
+    return "Invalid input. Please enter valid numeric values for width and height.";
+  }
+}
+const width = parseFloat(prompt("Enter the width of the rectangle:"));
+const height = parseFloat(prompt("Enter the height of the rectangle:"));
+const area = calculateRectangleArea(width, height);
+
+if (typeof area === 'number') {
+  alert(`The area of the rectangle with width ${width} and height ${height} is ${area.toFixed(2)}.`);
+} else {
+  alert(area);
+}
