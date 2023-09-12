770723da1b6ae5806f447d1f92e6c5cbef9d09f6
jamalmakavelii <126849635+jamalmakavelii@users.noreply.github.com>
2023-09-12 22:38:49 +0800

    Done

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,38 @@
-// Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+// Function to calculate and display the perimeter of a rectangle
+function calculateRectanglePerimeter(width, length) {
+  // Calculate the sum of width and length
+  var sum = width + length;
+  // Calculate the perimeter (twice the sum)
+  var perimeter = 2 * sum;
+
+  // Check if either width or length is not a number
+  if (isNaN(width) || isNaN(length)) {
+    return 'Error: Both values must be numbers';
+  }
+
+  // Return the result as a string
+  return "Perimeter: " + perimeter;
 }
 
+// Function to calculate and display the length of the diagonal of a rectangle
+function calculateLengthofDiagonal(length, width) {
+  // Calculate the square of width and length and their sum
+  var sumOfSquares = width ** 2 + length ** 2;
+  // Calculate the square root of the sum of squares
+  var diagonalLength = Math.sqrt(sumOfSquares);
+
+  // Check if either length or width is not a number
+  if (isNaN(length) || isNaN(width)) {
+    return 'Error: Both values must be numbers';
+  }
+
+  // Return the result as a string
+  return "Diagonal length: " + diagonalLength;
+}
+
+// Test the functions with numeric and non-numeric inputs
+console.log(calculateRectanglePerimeter(11, 11));
+console.log(calculateRectanglePerimeter("not a number"));
+
+console.log(calculateLengthofDiagonal(11, 11));
+console.log(calculateLengthofDiagonal("not a number"));
