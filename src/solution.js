23e78fe748eb156e1a9aad3cf72ec28a3b054422
Eliiij <142901296+Eliiij@users.noreply.github.com>
2023-09-14 23:24:47 +0800

    solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,5 +1,30 @@
 // Function to calculate the area of a rectangle
-function calculateRectangleArea(_, _) {
-  // Put your code here
+function calculateRectanglePerimeter(w, l) {
+
+  var a = w + l;
+  var p = 2 * a;
+
+  if(isNaN(w, l)) {
+    return 'Error: the entered value is not a number';
+  }
+
+  return "Perimeter is:" + p;
 }
 
+function calculateDiagonal(w, l) {
+
+  var a = w**2 + l**2;
+  var d = Math.sqrt(a);
+  
+  if(isNaN(w, l)){
+    return 'Error: the entered value is not a number';
+  }
+
+  return "Diagonal is: " + d;
+}
+
+console.log(calculateRectanglePerimeter(5, 5));
+console.log(calculateRectanglePerimeter("abc"));
+
+console.log(calculateDiagonal(5, 5));
+console.log(calculateDiagonal("abc"));
